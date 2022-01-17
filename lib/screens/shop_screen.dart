b5421694e36d34250b950e594041d860cf633f21
Rizky Ramadhan <jmiryas@gmail.com>
2022-01-17 16:17:39 +0700

    Add category list

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -46,100 +46,48 @@ class ShopScreen extends StatelessWidget {
               height: 25.0,
             ),
             SizedBox(
-              height: 65.0,
+              height: 95.0,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: iconCategoryList.map((item) {
-                  return Container(
-                    width: 65.0,
-                    height: 65.0,
-                    padding: const EdgeInsets.all(15.0),
-                    margin: const EdgeInsets.only(right: 20.0),
-                    child: Image(
-                      image: AssetImage(item.image),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-            const SizedBox(
-              height: 25.0,
-            ),
-            SizedBox(
-              height: 200.0,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: discountList.map((item) {
-                  return Container(
-                    width: 300.0,
-                    height: 200.0,
-                    padding: const EdgeInsets.all(20.0),
-                    margin: const EdgeInsets.only(right: 20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.5),
-                          BlendMode.dstIn,
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 65.0,
+                        height: 65.0,
+                        padding: const EdgeInsets.all(15.0),
+                        margin: const EdgeInsets.only(right: 20.0),
+                        child: Image(
+                          image: AssetImage(item.image),
                         ),
-                        image: AssetImage(item.background),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Image(
-                            width: 100.0,
-                            height: 100.0,
-                            image: AssetImage(item.image),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 65.0,
+                          height: 20.0,
+                          child: Center(
+                            child: Text(
+                              item.name,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
                         ),
-                        const SizedBox(
-                          width: 15.0,
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                item.title,
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                  color: Colors.orange.shade600,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                item.discount,
-                                style: TextStyle(
-                                  fontSize: 55.0,
-                                  color: Colors.orange.shade600,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                item.description,
-                                style: TextStyle(
-                                  color: Colors.green.shade600,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   );
                 }).toList(),
               ),
-            )
+            ),
           ],
         ),
       ),
