6b4ddae5d942c7c8f1f203427d138e7fd35008ec
Rizky Ramadhan <jmiryas@gmail.com>
2022-01-18 13:31:00 +0700

    Add discount dots

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:vegetable_market_app/data/data.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
-class ShopScreen extends StatelessWidget {
+import '../data/data.dart';
+
+class ShopScreen extends StatefulWidget {
   const ShopScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShopScreen> createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  int _currentDiscountIndex = 0;
+
+  List<Widget> buildDiscountDots(int currentIndex) {
+    List<Widget> discountDots = [];
+
+    for (int i = 0; i < discountList.length; i++) {
+      discountDots.add(CircleAvatar(
+        backgroundColor:
+            currentIndex == i ? Colors.green : Colors.grey.shade300,
+        radius: 4.0,
+      ));
+
+      discountDots.add(const SizedBox(
+        width: 5.0,
+      ));
+    }
+
+    return discountDots;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,73 +122,94 @@ class ShopScreen extends StatelessWidget {
               height: 200.0,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: discountList.map((item) {
-                  return Container(
-                    width: 300.0,
-                    height: 200.0,
-                    padding: const EdgeInsets.all(20.0),
-                    margin: const EdgeInsets.only(right: 20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.5),
-                          BlendMode.dstIn,
+                children: discountList.asMap().entries.map((item) {
+                  return VisibilityDetector(
+                      key: Key(item.key.toString()),
+                      child: Container(
+                        width: 300.0,
+                        height: 200.0,
+                        padding: const EdgeInsets.all(20.0),
+                        margin: const EdgeInsets.only(right: 20.0),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.5),
+                              BlendMode.dstIn,
+                            ),
+                            image: AssetImage(item.value.background),
+                          ),
                         ),
-                        image: AssetImage(item.background),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Image(
+                                width: 100.0,
+                                height: 100.0,
+                                image: AssetImage(item.value.image),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15.0,
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item.value.title,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.orange.shade600,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    item.value.discount,
+                                    style: TextStyle(
+                                      fontSize: 55.0,
+                                      color: Colors.orange.shade600,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    item.value.description,
+                                    style: TextStyle(
+                                      color: Colors.green.shade600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Image(
-                            width: 100.0,
-                            height: 100.0,
-                            image: AssetImage(item.image),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 15.0,
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                item.title,
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                  color: Colors.orange.shade600,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                item.discount,
-                                style: TextStyle(
-                                  fontSize: 55.0,
-                                  color: Colors.orange.shade600,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                item.description,
-                                style: TextStyle(
-                                  color: Colors.green.shade600,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  );
+                      onVisibilityChanged: (VisibilityInfo info) {
+                        if (info.visibleFraction == 1) {
+                          setState(() {
+                            _currentDiscountIndex = item.key;
+                          });
+                        }
+                      });
                 }).toList(),
               ),
             ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 15.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: buildDiscountDots(_currentDiscountIndex),
+              ),
+            )
           ],
         ),
       ),
